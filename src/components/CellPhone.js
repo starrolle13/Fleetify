b7fd7ba9c6cb84b8a76ea5ae43a9f459c6b4922d
starrolle13 <starnesharolle13@gmail.com>
2022-03-10 21:18:36 -0800

    phone section/header

--- src/components/CellPhone.js
+++ src/components/CellPhone.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
 export default function CellPhone() {
-  return <div className="cellContainer"></div>;
+  return (
+    <div className="cellContainer">
+      <div className="phoneSvgBox">
+        <div className="phoneSvg"></div>
+      </div>
+      <div className="rightBox">
+        <div className="cellTitle">
+          <h1>How's It Work?</h1>
+        </div>
+        <div className="cellSteps">
+          <p>1. Download</p>
+          <p>2. Install</p>
+          <p>3. Run</p>
+        </div>
+        <div className="cellInfo">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export default function Header() {
-  return <div className="headerBox">Header</div>;
+  return (
+    <div className="headerBox">
+      <div className="headerTextBox">
+        <div className="headerTitle">
+          <h1>Track All of Your Fleet Data</h1>
+        </div>
+        <div className="headerDescrip">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </div>
+        <button className="headerButton">Contact</button>
+      </div>
+    </div>
+  );
 }
