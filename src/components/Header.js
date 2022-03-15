83ab19ad78388373f074d300489b7f7576e49531
starrolle13 <starnesharolle13@gmail.com>
2022-03-14 18:24:09 -0700

    styled header/contact form

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ export default function Header() {
   return (
     <div className="headerBox">
       <div className="headerTextBox">
-        {/* <div className="headerTitle">
+        <div className="headerTitle">
           <h1>Track All of Your Fleet Data</h1>
         </div>
-        <div className="headerDescrip">
+        {/* <div className="headerDescrip">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        </div>
-        <button className="headerButton">Contact</button> */}
+        </div> */}
+        <button className="headerButton">Contact</button>
       </div>
     </div>
   );
